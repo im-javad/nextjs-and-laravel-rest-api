31f9be4e7d6133e8fa1d655ef4f3c1e107c76ced
JavadJadidi <javadjadidi@gmail.com>
2023-05-26 21:49:00 +0330

    create-auth-hooks"

---
+++ nextjs-frontend/hooks/auth.js
@@ -0,0 +1,127 @@
+import axios from "@/lib/axios";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+export const useAuth = ({ middleware, redirectIfAuthenticated } = {}) => {
+  const router = useRouter();
+
+  // isloading
+  // const [isLoading, setIsLoading] = useState(1);
+
+  // csrf
+  const csrf = () => axios.get("/sanctum/csrf-cookie");
+
+  // user
+  const {
+    data: user,
+    error,
+    mutate,
+  } = useSWR("/api/v1/user", () => {
+    axios
+      .get("/api/v1/user")
+      .then((response) => response.data.data)
+      .catch((error) => {
+        if (error.response.status !== 409) throw error;
+        router.push("/verify-email");
+      });
+  });
+
+  // login
+  const login = async ({ setErrors, setStatus, ...props }) => {
+    await csrf();
+
+    setErrors([]);
+    setStatus(null);
+
+    axios
+      .post("/login", ...props)
+      .then(() => mutate())
+      .catch((error) => {
+        if (error.response.status !== 422) throw error;
+
+        setErrors(error.response.data.error);
+      });
+  };
+
+  // logout
+  const logout = async () => {
+    if (!error) await axios.post("/logout").then(() => mutate());
+
+    window.location.pathname = "/login";
+  };
+
+  const register = async ({ setErrors, ...props }) => {
+    await csrf();
+
+    setErrors([]);
+
+    axios
+      .post("/register", props)
+      .then(() => mutate())
+      .catch((error) => {
+        if (error.response.status !== 422) throw error;
+        setErrors(error.response.data.errors);
+      });
+  };
+
+  const forgotPassword = async ({ setErrors, setStatus, email }) => {
+    await csrf();
+
+    setErrors([]);
+    setStatus(null);
+
+    axios
+      .post("/forgot-password", { email })
+      .then((response) => setStatus(response.data.status))
+      .catch((error) => {
+        if (error.response.status !== 422) throw error;
+        setErrors(error.response.data.errors);
+      });
+  };
+
+  const resetPassword = async ({ setErrors, setStatus, ...props }) => {
+    await csrf();
+
+    setErrors([]);
+    setStatus(null);
+
+    axios
+      .post("reset-password", props)
+      .then((response) =>
+        router.push("/login?reset=" + btoa(response.data.status))
+      )
+      .catch((error) => {
+        if (error.response.status !== 422) throw error;
+        setErrors(error.response.data.errors);
+      });
+  };
+
+  const resendEmailVerification = ({ setStatus }) => {
+    axios
+      .post("/email/verification-notification")
+      .then((response) => setStatus(response.data.status));
+  };
+
+  useEffect(() => {
+    // if(user || error)
+    //   setIsLoading(0);
+    if (middleware === "guest" && redirectIfAuthenticated && user)
+      router.push(redirectIfAuthenticated);
+    if (window.location.pathname === "/verify-email" && user?.email_verified_at)
+      router.push(redirectIfAuthenticated);
+    if (middleware === "auth" && error) logout();
+  }, [user, error]);
+
+  return {
+    isLoading,
+    csrf,
+    user,
+    login,
+    logout,
+    register,
+    forgotPassword,
+    resetPassword,
+    resendEmailVerification,
+  };
+};
---
+++ nextjs-frontend/lib/axios.js
@@ -0,0 +1,11 @@
+import Axios from "axios";
+
+const axios = Axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_API_BASIC_URL,
+  headers: {
+    "X-Requested-with": "XMLHttpRequest",
+  },
+  withCredentials: 1,
+});
+
+export default axios;
